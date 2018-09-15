2921d95f82bbf0ca0a6ae8a0feedd38d4c671507
Rezjiek <42703476+Rezjiek@users.noreply.github.com>
2018-09-15 15:03:30 +0200

    Update my neural network.py

--- my neural network.py
+++ my neural network.py
@@ -2,181 +2,67 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-#sigmoid squishification function
 def sig(x):
-  return 1/(1+np.exp(-x))
+    return 1/(1+np.exp(-x))
 
-
-#derivative of the sigmoid function
 def d_sig(x):
-  return 1/(2+2*np.cosh(x))
+    return x*(1-x)
 
 
-"""
-The network and gradient descent in a class.
-
-all weights -> self.ws
-all biases -> self.bs
-
-weight gradient -> w_grad
-bias gradient -> b_grad
-
-all inputs -> self.xs
-all (corresponding) outputs -> self.ys
-"""
 class Network:
 
-  def __init__(self,shape=()):
-    self.shape = shape
+    def __init__(self,shape=(),data=(None,None)):
+        self.lenght = len(shape)
+        self.shape = shape
 
-    self.ws = np.array([np.ones((i,j)) for i,j in zip(shape[1:],shape)])
-    self.bs = np.array([np.ones((i,1)) for i in shape[1:]])
+        self.xs = data[0]
+        self.ys = data[1]
 
-    self.w_grad = np.array([np.ones((i,j)) for i,j in zip(shape[1:],shape)])
-    self.b_grad = np.array([np.ones((i,1)) for i in shape[1:]])
+        self.ws = np.array([np.zeros((i,j)) for i,j in zip(shape[1:],shape)])
+        self.bs = np.array([np.zeros((i,1)) for i in shape[1:]])
 
-    self.xs = self
-    self.ys = self
+        self.w_grad = np.array([np.zeros((i,j)) for i,j in zip(shape[1:],shape)])
+        self.b_grad = np.array([np.zeros((i,1)) for i in shape[1:]])
 
+        self.tot_grad = np.array([[np.zeros((i,j)) for i,j in zip(shape[1:],shape)],[np.zeros((i,1)) for i in shape[1:]]])
 
 
-  #function where you pass an input (x) through the neural network
-  def neur_sum(self,x):
+    def neur_sum(self,x):
 
-    for w,b in zip(self.ws,self.bs):
+        for w,b in zip(self.ws,self.bs):
+            x = sig(w@x+b)
 
-      x = sig(w@x+b)
+        return x
 
-    return x
 
+    def n_grad(self,n):
+        x = self.xs[n]
+        y = self.ys[n]
 
+        dc_dx = [0]*self.lenght
 
-  #cost function
-  #so it gives back how well the network is doing, higher means it's doing worse and vise versa
-  def cost(self,n):
+        all_x = [x]
+        for w,b in zip(self.ws,self.bs):
+            x = sig(w@x+b)
+            all_x.append(x)
 
-    out = 0
-    for x,y in zip(self.neur_sum(self.xs[n]),self.ys[n]):
-      out += np.sum((x-y)**2)
+        dc_dx[-1] = x-y
 
-    return out
+        for L in range(self.lenght-2,-1,-1):
+            dc_dx[L] = self.ws[L].T@(dc_dx[L+1]*d_sig(all_x[L+1]))
+            self.b_grad[L] = dc_dx[L+1]*d_sig(all_x[L+1])
+            self.w_grad[L] = self.b_grad[L]@all_x[L].T
 
 
+    def gradient(self):
 
-  #creates the necessary numbers used for calculating all partials (for the gradient)
-  def create_partials(self,n):
+        for n in range(len(self.xs)):
 
-    a = np.array([np.zeros((i)) for i in self.shape])
-    dz = np.array([np.zeros((i)) for i in self.shape[1:]])
+            self.n_grad(n)
+            w = self.ws-self.w_grad
+            b = self.bs-self.b_grad
 
-    t = 0
-    a[0] = self.xs[n]
-    for w,b in zip(self.ws,self.bs):
-      t += 1
-      a[t] = sig(w@a[t-1]+b)
-      dz[t-1] = d_sig(w@a[t-1]+b)
-
-    return a,dz
-
-
-
-  #calculates the gradient for all weights and biases
-  def gradient(self,n):
-    a,dz = self.create_partials(n)
-
-
-    dc_dx = np.array([np.zeros((i)) for i in self.shape])
-    max_L = len(self.shape)-1
-
-
-    for i,j in zip(self.neur_sum(self.xs[n]),self.ys[n]):
-      dc_dx[max_L] = 2*(i-j)
-
-
-    for L in range(max_L-1,-1,-1):
-
-      for row in range(self.shape[L]):
-
-        dc_dx[L][row] = sum([dc_dx[L+1][t]*dz[L][t]*self.ws[L][t][row] for t in range(self.shape[L+1])])
-
-      for row in range(self.shape[L+1]):
-        self.b_grad[L][row][0] = dc_dx[L+1][row]*dz[L][row]
-
-        for col in range(self.shape[L]):
-          self.w_grad[L][row][col] = self.b_grad[L][row][0]*a[L][col]
-
-
-
-    return self.w_grad,self.b_grad
-
-
-
-  #keeps lowering the cost by subtracting the gradient (gradient descent)
-  def lower_cost(self,loops):
-    c = 1
-
-    out_cost = []
-    for i in range(loops):
-      w,b = 0,0
-      sum_cost = 0
-      for n in range(len(self.xs)):
-        sum_cost += k.cost(n)
-        w0,b0 = self.gradient(n)
-        w,b = w+w0,b+b0
-
-      out_cost.append(sum_cost)
-      self.ws -= c*w
-      self.bs -= c*b
-
-    return out_cost
-
-
-def img_to_array(img):
-
-  out = []
-  for i,j in np.ndenumerate(img):
-    out.append([j])
-
-  return np.array(out)
-
-
-def all_data(all_urls):
-  out = []
-  for i in all_urls:
-    out.append(img_to_array(plt.imread(i)[:,:,0]))
-
-  return out
-
-
-#you can change the paths to how you have it set up
-paths = [
-"C:/Users/Midas/Documents/programmeren/School/Python-supervised-deep-learning-algorithm-master/data/empty.png",
-"C:/Users/Midas/Documents/programmeren/School/Python-supervised-deep-learning-algorithm-master/data/filled.png"
-]
-
-
-k = Network(shape=(256,15,15,1))
-
-
-data = all_data(urls)
-
-k.xs = np.array( data )
-k.ys = np.array([ [[0]],[[1]] ])
-
-
-tot_cost = np.array(k.lower_cost(1000))
-time = np.arange(1000)
-
-plt.plot(time,tot_cost)
-
-
-
-
-
-
-
-
-
+        return w,b
 
 
 
