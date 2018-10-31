41f6e1c61c0a36a5a25a836faa12e546c77e7f5d
Rezjiek <42703476+Rezjiek@users.noreply.github.com>
2018-10-31 17:04:47 +0100

    Update my neural network.py

--- my neural network.py
+++ my neural network.py
@@ -1,74 +1,85 @@
+import data
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 
 
 def sig(x):
     return 1/(1+np.exp(-x))
 
 def d_sig(x):
-    return x*(1-x)
+    return sig(x)*(1-sig(x))
 
 
 class Network:
 
-    def __init__(self,shape=(),data=(None,None)):
-        self.lenght = len(shape)
+    def __init__(self,shape=(),data=(None,None),funct=sig,d_funct=d_sig):
+        self.f = funct
+        self.d_f = d_funct
+
+        self.length = len(shape)
         self.shape = shape
 
-        self.xs = data[0]
-        self.ys = data[1]
-
-        self.ws = np.array([np.zeros((i,j)) for i,j in zip(shape[1:],shape)])
-        self.bs = np.array([np.zeros((i,1)) for i in shape[1:]])
+        self.all_x = np.array([np.zeros((i,1)) for i in shape[:]])
+        self.all_dz = np.array([np.zeros((i,1)) for i in shape[1:]])
 
         self.w_grad = np.array([np.zeros((i,j)) for i,j in zip(shape[1:],shape)])
         self.b_grad = np.array([np.zeros((i,1)) for i in shape[1:]])
 
-        self.tot_grad = np.array([[np.zeros((i,j)) for i,j in zip(shape[1:],shape)],[np.zeros((i,1)) for i in shape[1:]]])
+        self.xs = np.array(data[0])
+        self.ys = np.array(data[1])
+
+        self.ws = np.array([np.zeros((i,j)) for i,j in zip(shape[1:],shape)])
+        self.bs = np.array([np.zeros((i,1)) for i in shape[1:]])
+
+
+    def cost(self,xs,ys):
+        return np.sum((self.neur_sum(xs)-ys)**2)/(len(xs)*len(ys[0]))
 
 
     def neur_sum(self,x):
 
         for w,b in zip(self.ws,self.bs):
-            x = sig(w@x+b)
+            x = self.f(w@x+b)
 
         return x
 
 
-    def n_grad(self,n):
-        x = self.xs[n]
-        y = self.ys[n]
+    def gradient(self,x,y):
 
-        dc_dx = [0]*self.lenght
-
-        all_x = [x]
+        L = 0
+        self.all_x[L] = x
         for w,b in zip(self.ws,self.bs):
-            x = sig(w@x+b)
-            all_x.append(x)
-
-        dc_dx[-1] = x-y
-
-        for L in range(self.lenght-2,-1,-1):
-            dc_dx[L] = self.ws[L].T@(dc_dx[L+1]*d_sig(all_x[L+1]))
-            self.b_grad[L] = dc_dx[L+1]*d_sig(all_x[L+1])
-            self.w_grad[L] = self.b_grad[L]@all_x[L].T
+            z = w@x+b
+            self.all_dz[L] = self.d_f(z)
+            x = self.f(z)
+            self.all_x[L+1] = x
+            L += 1
 
 
-    def gradient(self):
+        dc_dx = x-y
+        
+        L = self.length-2
+        while L >= 0:
+            dc_db = dc_dx*self.all_dz[L]
+            dc_dw = dc_db@self.all_x[L].T
+            dc_dx = self.ws[L].T@(dc_dx*self.all_dz[L])
+            self.w_grad[L] = dc_dw
+            self.b_grad[L] = dc_db
+            L -= 1
 
-        for n in range(len(self.xs)):
-
-            self.n_grad(n)
-            w = self.ws-self.w_grad
-            b = self.bs-self.b_grad
-
-        return w,b
-
+        return self.w_grad,self.b_grad
 
 
 
+test = Network(shape=(784,15,10),data=(data.images,data.labels))
 
+print(test.cost(np.array(data.images[:1000]),np.array(data.labels[:1000])))
 
-
+for i in range(1000):
+    for x,y in zip(np.array(data.images[:1000]),np.array(data.labels[:1000])):
+        w,b = test.gradient(x,y)
+        test.ws -= w
+        test.bs -= b
 
 
