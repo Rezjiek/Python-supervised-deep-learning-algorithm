ebc9129dc4bac1803807dc42c6dc5f87adcce0f3
Rezjiek <42703476+Rezjiek@users.noreply.github.com>
2018-11-30 22:58:29 +0100

    Update data.py

--- data.py
+++ data.py
@@ -10,26 +10,25 @@ images,labels = data.load_training()
 images = np.array(images)
 labels = np.array(labels)
 
-
-def c_img(img):
-    return img.reshape((784,1))/255.0
-
 def c_lbl(lbl):
     out = np.zeros((10,1))
     out[lbl-1] = 1
     return out
 
 
-out_images = []
 out_labels = []
 for img,lbl in zip(images,labels):
-    out_images.append(c_img(np.array(img)))
     out_labels.append(c_lbl(lbl))
 
-images = out_images
-labels = out_labels
 
-images = np.array(out_images)
+images = images.reshape(60000,784,1)/255.0
 labels = np.array(out_labels)
+
+all_data = []
+for i,l in zip(images,labels):
+    all_data.append((i,l))
+
+
+
 
 
